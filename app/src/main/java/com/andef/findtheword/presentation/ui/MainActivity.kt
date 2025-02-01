2482f247e1953e52fa3ecc71c28f8a6db9fac55f
Дмитрий Семкин <semkin_dmitriy10@vk.com>
2025-02-01 12:55:25 +0400

    Finished project. Add random words

--- app/src/main/java/com/andef/findtheword/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/andef/findtheword/presentation/ui/MainActivity.kt
@@ -84,6 +84,9 @@ class MainActivity : AppCompatActivity() {
             text = getString(R.string.word_not_found_continue)
             gravity = Gravity.CENTER
         }
+        backgroundView.findViewById<Button>(R.id.buttonRandomWord).apply {
+            visibility = GONE
+        }
         backgroundView.findViewById<EditText>(R.id.editTextDialog).apply {
             visibility = GONE
         }
